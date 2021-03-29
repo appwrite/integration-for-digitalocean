363d463c2bae72a266a1b39ead542597e9b059a7
Damodar Lohani <dlohani48@gmail.com>
2021-03-29 08:26:09 +0545

    install 0.7.2 version

--- files/opt/appwrite/install.sh
+++ files/opt/appwrite/install.sh
@@ -8,6 +8,6 @@ docker run -it --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
     --entrypoint="install" \
-    appwrite/appwrite:0.7.0 --version 0.7.0
+    appwrite/appwrite:0.7.2
 
 cp -f /etc/skel/.bashrc /root/.bashrc
