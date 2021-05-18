0f5d2cd2840f3a73264e46d7dca362be87ebe6e3
Damodar Lohani <dlohani48@gmail.com>
2021-05-18 18:45:07 +0545

    adding swap for making it work with low spec system

--- scripts/02-setup-appwrite-scripts.sh
+++ scripts/02-setup-appwrite-scripts.sh
@@ -9,8 +9,6 @@ echo "==============================================="
 echo "=             Pulling docker images             ="
 echo "==============================================="
 
-echo "pulling appwrite docker images"
-
 docker pull dlohani/appwrite:0.8.0
 docker pull traefik:2.3
 docker pull appwrite/mariadb:1.2.0
@@ -18,3 +16,19 @@ docker pull redis:6.0-alpine3.12
 docker pull influxdb:1.8-alpine
 docker pull appwrite/telegraf:1.0.0
 
+
+echo "==============================================="
+echo "=             Setting up Swap                 ="
+echo "==============================================="
+
+fallocate -l 2G /swapfile
+ls -lh /swapfile
+
+chmod 600 /swapfile
+ls -lh /swapfile
+
+mkswap /swapfile
+swapon /swapfile
+swapon --show
+
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
