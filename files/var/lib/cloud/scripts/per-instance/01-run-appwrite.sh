82cf5657db29543f874563836e2e68d44dea14cb
Damodar Lohani <dlohani48@gmail.com>
2022-05-19 04:07:41 +0000

    update appwrite image

--- files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
+++ files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
@@ -5,4 +5,4 @@ cd /root
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
-    appwrite/appwrite:0.14.0 sh -c "install --httpPort=80 --httpsPort=443 --interactive=N"
+    appwrite/appwrite:0.14.1 sh -c "install --httpPort=80 --httpsPort=443 --interactive=N"
--- scripts/02-setup-appwrite-scripts.sh
+++ scripts/02-setup-appwrite-scripts.sh
@@ -9,7 +9,7 @@ echo "==============================================="
 echo "=             Pulling docker images             ="
 echo "==============================================="
 
-docker pull appwrite/appwrite:0.14.0
+docker pull appwrite/appwrite:0.14.1
 docker pull traefik:2.7
 docker pull mariadb:10.7
 docker pull redis:6.2-alpine
