ca7c9c4039fbcdd9425c4a629baa03902c3ff71b
Damodar Lohani <dlohani48@gmail.com>
2023-04-12 09:38:10 +0000

    updated 1.3.0

--- files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
+++ files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
@@ -5,4 +5,4 @@ cd /root
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
-    appwrite/appwrite:1.2.1 sh -c "install --httpPort=80 --httpsPort=443 --interactive=N"
+    appwrite/appwrite:1.3.0 sh -c "install --httpPort=80 --httpsPort=443 --interactive=N"
