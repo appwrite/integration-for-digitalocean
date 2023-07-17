5898ff15165f38d7ce2f8d42eddc7002af4f7b4c
Damodar Lohani <dlohani48@gmail.com>
2023-07-17 03:12:52 +0000

    information

--- files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
+++ files/var/lib/cloud/scripts/per-instance/01-run-appwrite.sh
@@ -4,6 +4,11 @@ cd /root
 
 appversion=$(curl -s https://api.github.com/repos/appwrite/appwrite/releases/latest | grep -oP '"tag_name": "\K.*?(?=")')
 
+echo "==============================================="
+echo "=       Installing appwrite $appversion       ="
+echo "==============================================="
+
+
 docker run --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     --volume "$(pwd)"/appwrite:/usr/src/code/appwrite:rw \
